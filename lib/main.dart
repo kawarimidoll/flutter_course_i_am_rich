4a4f9c3ce34cc98ea2b85bf5cb888b47858777c3
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-21 20:05:37 +0900

    create app that show 'Hello World!'

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The main() function in mian.dart is the entrypoint for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World!'),
+      ),
+    ),
+  );
+}
