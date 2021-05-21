717109cadcae0bc7e28788ef22cef539b9e4c6cb
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-21 20:32:43 +0900

    add NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
+var imageUrl =
+    'https://raw.githubusercontent.com/kawarimidoll/kawarimidoll/master/assets/kawarimiku_v4x.png';
 // The main() function in mian.dart is the entrypoint for all our Flutter apps.
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(imageUrl),
+          ),
+        ),
       ),
     ),
   );
